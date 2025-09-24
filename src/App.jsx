5a00ac921b655d5e20805dc1faef7066e098b826
Hisu <tanquochao1123@gmail.com>
2025-09-24 19:12:44 +0700

    Update navbar and app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import Dashboard from "./components/admin/DashBoard";
+import Login from "./components/login/Login";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
+
+
