55f59a9f37874bfa64ed3354cc7bd06efb0a81dd
Hisu <tanquochao1123@gmail.com>
2025-10-02 08:19:36 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import Dashboard from "./components/admin/DashBoard";
 import Login from "./components/login/Login";
-
+import Overview from "./components/evm/Overview";
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/overview" element={<Overview />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
